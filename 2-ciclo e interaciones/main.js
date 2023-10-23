90de2cd4e56640428112e9f0615831bc5f0cea8e
maktub <maktubdesarrolloweb@gmail.com>
2023-10-23 16:09:55 -0300

    ejemplos

---
+++ 2-ciclo e interaciones/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+/*let nombre = prompt("ingresa tu nombre")*/
+
+//condicion,true,falso
+
+/*
+if(nombre == "carlos"){
+    alert("bienvenido crack")
+}else{
+    alert("anda pa´lla bobo")
+}
+*/
+
+
+
+/*let gaston = true
+
+if (gaston){
+    alert("gaston esta en la clase")
+}
+*/
+
+
