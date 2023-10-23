4074fab674e539d45229b14267dbca654628e6ef
maktub <maktubdesarrolloweb@gmail.com>
2023-10-23 15:06:57 -0300

    control de flujos

---
+++ 1-variables/main.js
@@ -0,0 +1,72 @@
+
+
+/* sintaxis para declarar una variable*/
+//tipo, nombre = dato
+
+//var mate = "yerba"  //esto no nes recomendado no se usa mas 
+
+
+//let vaso = "agua"
+
+
+
+//const termo = "agua caliente" //no cambia el valor porque es una constante
+
+//const pi = 3.14
+
+//string, es un tipo de dato que pertenece a una cadena de caracteres (osea letras, simbolos, etc)
+//number, es un tipo de dato que pertenece a numeros
+//bool, es un tipo de dato que solo representa VERDADERO o FALSO
+
+
+//let numeroA = 1 
+//let numeroB = 2 
+//let numeroC = 3
+//Suma de dos numeros (1+2=3)
+//let resultadoSuma = numeroA + numeroB
+
+//Resta de dos numeros (2-1=1)
+//let resultadoResta = numeroB - numeroA
+
+//Producto de dos números (2*3=6)
+//let resultadoProducto = numeroB * numeroC
+
+
+
+//let nombre = "Jesus"
+//let apellido = "Barbona"
+
+//let nombreCompleto = nombre +" "+ apellido //concatenar es la sumatoria de diferentes datos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert( nombreCompleto) //dispara un cartel de alerta en mi pantalla 
+
+
+
+
+                    //CONSOLE, PROMPT, ALERT
+
+let nombreUsuario = prompt("Si seguis te llevas una sorpresa")
+
+alert("sos 🤘 " + nombreUsuario)
+
+console.log("puto el que lee")
+console.warn("ojo que en esta parte hay problemas")
+console.error("aca te equivaste feo papi")
+
+
+
+
